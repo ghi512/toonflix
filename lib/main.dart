9ab212b56bcc73ab6fd0e7fea2bc4395169a76bb
김민지 <ghi512151@gmail.com>
2025-04-09 15:34:08 +0900

    #5 - 5장 시작 전 코드 정리

--- lib/main.dart
+++ lib/main.dart
@@ -15,33 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: TextTheme(titleLarge: TextStyle(color: Colors.red)),
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [MyLargeTitle()],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatelessWidget {
-  const MyLargeTitle({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      home: Scaffold(backgroundColor: Colors.white, body: Center()),
     );
   }
 }
